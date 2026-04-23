06cccb546405f19d88557f0fd3e1477fc810c302
Tung HOANG <tung.hoang@gmail.com>
2026-04-23 09:42:42 +0700

    changes

--- namecard/form-handling.php
+++ namecard/form-handling.php
@@ -3,5 +3,10 @@
 <?php
 print("Form handled");
 ?>
+<div><h3>data sent from browser (querystring)</h3>
+<?= $_GET['fullName'] ?>
+<?= $_GET['gender'] ?>
+<?= $_GET['position'] ?>
+</div>
 </body>
 </html>
