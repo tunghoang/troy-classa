ce3b51844a750d9ccde90a645d985de1f8973c4f
Tung HOANG <tung.hoang@gmail.com>
2026-04-25 01:44:27 +0000

    update

--- admin-area/newuser.php
+++ admin-area/newuser.php
@@ -18,10 +18,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     print($_POST['username']);
     print($_POST['fullname']);
     print($_POST['password']);
+
+    // Create a user using the above information 
+    
+    $servername = "127.0.0.1";
+    $username = "admin";
+    $password = "Abc123";
+    $dbname = "socialnetA";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    $stmt = $conn->prepare("INSERT INTO account (username, fullname, password, description) VALUES (?, ?, ?, ?)");
+
+    $user = $_POST['username'];
+    $full = $_POST['fullname'];
+    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT); // Always hash passwords!
+    $desc = "Hello guys!";
+    $stmt->bind_param("ssss", $user, $full, $pass, $desc);
+
+    // 4. Execute the statement
+    if ($stmt->execute()) {
+        echo "New account created successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // 5. Close statement and connection
+    $stmt->close();
+    $conn->close();
+
 }
 ?>
     <h2>User Registration</h2>
-    <form action="/newuser.php" method="POST">
+    <form action="newuser.php" method="POST">
         <div class="form-group">
             <label for="username">Username (max 20 characters):</label>
             <input type="text" id="username" name="username" maxlength="20" required>
